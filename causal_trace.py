cb3245a130276de47167e42a981cfa238041aeed
Sviatoslav <slava.chalnev@gmail.com>
2023-08-17 18:00:20 +1000

    Add mlp

--- causal_trace.py
+++ causal_trace.py
@@ -60,6 +60,7 @@ def corrupt_and_patch():
     def analyze_patch(model, tokens, layer_to_patch, position_to_patch):
         h_patch_place = f'blocks.{layer_to_patch}.hook_resid_post'
         att_patch_place = f'blocks.{layer_to_patch}.hook_attn_out'
+        mlp_patch_place = f'blocks.{layer_to_patch}.hook_mlp_out'
 
         def patch_h_hook(value, hook):
             value[:, position_to_patch] = clean_run_cache[h_patch_place][:, position_to_patch]
@@ -67,9 +68,15 @@ def corrupt_and_patch():
         
         def patch_att_hook(value, hook):
             value[:, position_to_patch] = clean_run_cache[att_patch_place][:, position_to_patch]
+            return value
+        
+        def patch_mlp_hook(value, hook):
+            value[:, position_to_patch] = clean_run_cache[mlp_patch_place][:, position_to_patch]
+            return value
         
         patch_hooks = [(f'hook_embed', add_noise), (h_patch_place, patch_h_hook)]
         attn_patch_hooks = [(f'hook_embed', add_noise), (att_patch_place, patch_att_hook)]
+        mlp_patch_hooks = [(f'hook_embed', add_noise), (mlp_patch_place, patch_mlp_hook)]
 
         with model.hooks(fwd_hooks=patch_hooks), torch.no_grad():
             h_patched_logits = model(tokens, return_type='logits')
@@ -77,10 +84,14 @@ def corrupt_and_patch():
         with model.hooks(fwd_hooks=attn_patch_hooks), torch.no_grad():
             att_patched_logits = model(tokens, return_type='logits')
         
+        with model.hooks(fwd_hooks=mlp_patch_hooks), torch.no_grad():
+            mlp_patched_logits = model(tokens, return_type='logits')
+        
         h_patch_prob = torch.softmax(h_patched_logits[0, -1], dim=-1)[target_token]
         att_patch_prob = torch.softmax(att_patched_logits[0, -1], dim=-1)[target_token]
+        mlp_patch_prob = torch.softmax(mlp_patched_logits[0, -1], dim=-1)[target_token]
 
-        return h_patch_prob, att_patch_prob
+        return h_patch_prob, att_patch_prob, mlp_patch_prob
 
 
     n_layers = model.cfg.n_layers
@@ -103,25 +114,30 @@ def corrupt_and_patch():
 
     h_diff_matrix = np.zeros((n_positions, n_layers))
     a_diff_matrix = np.zeros((n_positions, n_layers))
+    m_diff_matrix = np.zeros((n_positions, n_layers))
     for res in results:
-        layer_to_patch, position_to_patch, (h, a) = res
+        layer_to_patch, position_to_patch, (h, a, m) = res
         h_diff = np.abs(original_prob - h)
         a_diff = np.abs(original_prob - a)
+        m_diff = np.abs(original_prob - m)
         h_diff_matrix[position_to_patch, layer_to_patch] = h_diff
         a_diff_matrix[position_to_patch, layer_to_patch] = a_diff
+        m_diff_matrix[position_to_patch, layer_to_patch] = m_diff
     
-    return original_prob, corrupted_prob, h_diff_matrix.T, a_diff_matrix.T
+    return original_prob, corrupted_prob, h_diff_matrix.T, a_diff_matrix.T, m_diff_matrix.T
 
 
 # run n times and average
 h_diffs = []
 a_diffs = []
+m_diffs = []
 original_probs = []
 corrupted_probs = []
 for i in range(1):
-    original_prob, corrupted_prob, h_diff, a_diff = corrupt_and_patch()
+    original_prob, corrupted_prob, h_diff, a_diff, m_diff = corrupt_and_patch()
     h_diffs.append(h_diff)
     a_diffs.append(a_diff)
+    m_diffs.append(m_diff)
     original_probs.append(original_prob)
     corrupted_probs.append(corrupted_prob)
 
@@ -130,6 +146,7 @@ for i in range(1):
 # Average the differences
 h_diff_matrix = np.mean(h_diffs, axis=0)
 a_diff_matrix = np.mean(a_diffs, axis=0)
+m_diff_matrix = np.mean(m_diffs, axis=0)
 original_prob = np.mean(original_probs)
 corrupted_prob = np.mean(corrupted_probs)
 
@@ -138,7 +155,7 @@ x_labels = [str(i) if i != s_token_len - 1 else str(i) + '*' for i in range(toke
 vmin_vmax = {'vmin': 0, 'vmax': np.abs(original_prob - corrupted_prob)}
 
 # Create a figure with side-by-side subplots
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5))
 
 # Plot for h_diff
 ax1.set_xticks(range(tokens.shape[1]))
@@ -159,6 +176,17 @@ fig.colorbar(im2, ax=ax2, label='Difference')
 ax2.set_ylabel('Layer')
 ax2.set_xlabel('Position')
 ax2.set_title('Original - A_Patched')
+
+# Plot for m_diff
+ax3.set_xticks(range(tokens.shape[1]))
+ax3.set_xticklabels(x_labels)
+im3 = ax3.imshow(m_diff_matrix, aspect='auto', cmap='viridis', **vmin_vmax)
+ax3.invert_yaxis()
+fig.colorbar(im3, ax=ax3, label='Difference')
+ax3.set_ylabel('Layer')
+ax3.set_xlabel('Position')
+ax3.set_title('Original - M_Patched')
+
 
 # Save the figure
 plt.tight_layout()
