ebeeca5679a78ba44ac9f5dc6a6bb1d3d45fdc70
Sviatoslav <slava.chalnev@gmail.com>
2023-09-02 12:40:36 +1000

    Training loop

--- find_noise.py
+++ find_noise.py
@@ -2,6 +2,7 @@ from typing import List
 
 import torch
 import torch.nn.functional as F
+from torch.optim import Adam
 
 from transformer_lens import HookedTransformer
 from token_print import ColoredTokenizer
@@ -13,6 +14,7 @@ def find_noise(
         tokens: torch.Tensor,
         noise_idxs: List,
         noise_sd: float,
+        steps: int = 10,
     ):
     """Find noise which minimizes loss of tokens[:max(noise_idxs) + 1]
     while keeping the standard deviation of the noise equal to noise_sd.
@@ -24,23 +26,32 @@ def find_noise(
     remaining_zeros = torch.zeros((tokens.shape[-1] - len(noise_idxs), model.cfg.d_model))
     noise = torch.cat([noise, remaining_zeros], dim=0)
 
+    mask = torch.zeros_like(noise)
+    mask[:len(noise_idxs), :] = 1
+    mask = mask.to(model.cfg.device)
+
     noise = noise.to(model.cfg.device)
     noise.requires_grad = True
 
+    optimizer = Adam([noise], lr=0.1)
+
     def add_noise(value, hook):
         return value + noise
-
     noise_hooks = [(f'hook_embed', add_noise)]
-    with model.hooks(fwd_hooks=noise_hooks):
-        logits = model(tokens, return_type='logits')
-    
-    # loss averaged over all tokens after noise_idxs
-    targets = tokens[0, max(noise_idxs) + 1:].to(model.cfg.device)
-    ct(targets)
 
-    loss = F.cross_entropy(logits[0, max(noise_idxs):-1], target=targets)
-    print(loss)
-    
+    for _ in range(steps):
+        optimizer.zero_grad()
+        with model.hooks(fwd_hooks=noise_hooks):
+            logits = model(tokens, return_type='logits')
+        
+        # loss averaged over all tokens after noise_idxs
+        targets = tokens[0, max(noise_idxs) + 1:].to(model.cfg.device)
+        loss = F.cross_entropy(logits[0, max(noise_idxs):-1], target=targets)
+
+        loss.backward()
+        noise.grad *= mask
+        print(loss.item())
+        optimizer.step()
 
 
 if __name__ == '__main__':
